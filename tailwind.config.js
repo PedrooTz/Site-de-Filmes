4df58e3ea52700512f1ccab90e8e963bf28e0bc4
Pedro Henrique <pedrohenrique1603@gmail.com>
2024-03-05 16:55:39 -0300

    Iniciado

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secundary: '#531212',
+        terciary: '#FFFFFF'
+      }
+    },
+  },
+  plugins: [],
+}
+
